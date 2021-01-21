41d1b5cfdf7e0fbc344c96dd77471123ef58cdd8
Ian Patrick M Tan <ianpatricktan@gmail.com>
2021-01-21 00:25:41 -0500

    Update NeuralNetwork.py
    Due to my lack of experience I staged the wrong file in github here is the updated version with the actual code.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,9 +2,21 @@
 
 """
 
-#import numpy as np
-print(numpy.array([1, 2, 3]))
-__name__ = 'NeuralNetwork'
+import numpy, copy
 
-#def neuralNetwork(inputs, weights, ):
-    #np.vstack((a.transpose(), n.array([1, 1, 1]))).transpose()
+def ReLU(weightedInputs):
+    return numpy.append(weightedInputs, [0]).max()
+
+def neuralNetwork(initialInputs, weights, activationFunction, activationFunctionArguments):
+    neuronOutputs = []
+    inputs = copy.deepcopy(initialInputs)
+    for layerWeights in weights:
+        outputs = []
+        biasedInputs = numpy.vstack((inputs.transpose(), numpy.array([1 for x in inputs]))).transpose()
+        layerWeightedInputs = biasedInputs * layerWeights
+        for weightedInputs in  layerWeightedInputs:
+            outputs.append(activationFunction(weightedInputs, *activationFunctionArguments))
+        outputs = numpy.array(outputs)
+        neuronOutputs.append(outputs)
+        inputs = copy.deepcopy(outputs)
+    return neuronOutputs
