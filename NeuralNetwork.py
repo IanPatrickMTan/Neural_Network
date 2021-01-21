fe7cf67b9c12443beed26e8a3954fd7308b3668e
Ian Patrick M Tan <ianpatricktan@gmail.com>
2021-01-21 00:23:45 -0500

    Created NeuralNetwork.py
    I created NeuralNetwork.py and wrote a bit of code for the basics though this will almost certainly be subject to audit.

---
+++ NeuralNetwork.py
@@ -0,0 +1,10 @@
+"""
+
+"""
+
+#import numpy as np
+print(numpy.array([1, 2, 3]))
+__name__ = 'NeuralNetwork'
+
+#def neuralNetwork(inputs, weights, ):
+    #np.vstack((a.transpose(), n.array([1, 1, 1]))).transpose()
