111b6f347efd08d661cea972a5f5c797a0f04caa
Ian Patrick M Tan <ianpatricktan@gmail.com>
2021-01-22 02:20:37 -0500

    Updated NeuralNetwork.py
    I had an idea for a new object that may give the user more freedom and flexibility when designing different neural network architectures, though I am still new and this may not be removed in future updates.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -7,6 +7,13 @@ import numpy, copy
 def ReLU(weightedInputs):
     return numpy.append(weightedInputs, [0]).max()
 
+class layerOutput:
+    """
+    This should act as a data type for the output of a layer, this should carry information such as the output of the neurons some information we may wish to log as well as extra paramters we need to carry over to the next iteration
+    """
+    def __init__(self):
+        pass
+
 def neuralNetwork(initialInputs, weights, activationFunction, activationFunctionArguments):
     neuronOutputs = []
     inputs = copy.deepcopy(initialInputs)
