b5c852710e85eea2199997ecc9e9f37905896219
Ian Patrick M Tan <ianpatricktan@gmail.com>
2021-01-22 19:14:55 -0500

    Updated NeuralNetwork.py
    I did a bit of work off camera.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -11,8 +11,8 @@ class layerOutput:
     """
     This should act as a data type for the output of a layer, this should carry information such as the output of the neurons some information we may wish to log as well as extra paramters we need to carry over to the next iteration
     """
-    def __init__(self):
-        pass
+    def __init__(self, neuronOutputs):
+        self.neuronOutputs = neuronOutputs
 
 def neuralNetwork(initialInputs, weights, activationFunction, activationFunctionArguments):
     neuronOutputs = []
